7a5c8e5d8e6c5311c0320eadc5c0c51ce4a8b944
thucuyen127 <74052168+thucuyen127@users.noreply.github.com>
2021-04-26 10:42:16 +0700

    Add files via upload

---
+++ ex_26.R
@@ -0,0 +1,35 @@
+setwd('C:\\Users\\DELL')
+#1/
+answer <- 0
+for (j in 3: 5) {answer <- j + answer}
+answer 
+answer <- 10
+for (j in 3:5){ answer <- j+answer }
+answer <- 10
+for (j in 3:5){ answer <- j*answer }
+answer
+#2/
+help(prod)
+prod(3:5,10, na.rm = FALSE)
+#3/su <- 0
+for (i in 1:100){su <- su + i }
+su
+sum(1:100, na.rm = FALSE)
+#4/
+po <- 1
+for (i in 1:50){po <- po * i}
+po
+prod(1:50, na.rm = FALSE)
+#5/
+radius <- (3:20)
+volume <- (4/3*pi*(radius**3))
+conversion <- data.frame(radius=radius, volume=volume)
+print(conversion)
+#6/)
+help(sapply)
+oww <- read.table('Vegetation2.txt')
+tinting <- oww
+sapply(tinting, factor)
+sapply(tinting[, 1:6], levels)
+sapply(tinting[, 1:6], ordered)
+
